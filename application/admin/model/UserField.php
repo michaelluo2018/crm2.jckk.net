7f69901a2437ed67b788395221e7055b6b6721a4
Xmzz <2253948021@qq.com>
2019-04-04 14:22:07 +0800

    1

--- application/admin/model/UserField.php
+++ application/admin/model/UserField.php
@@ -31,6 +31,7 @@ class UserField extends Model
      */
     public function updateConfig($types, $param, $id = '')
     {
+        dump($param['value']);die();
         if (!is_array($param['value']) || !is_array($param['hide_value'])) {
             $this->error = '参数错误';
             return false;
