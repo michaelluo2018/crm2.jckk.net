b3f18cbab79e61f75407789e4fd8d7221d12604d
Xmzz <2253948021@qq.com>
2019-04-04 09:53:13 +0800

    1

--- application/admin/model/User.php
+++ application/admin/model/User.php
@@ -107,7 +107,6 @@ class User extends Common
 				->order($exp)
 				->order('user.id asc')
 				->select();
-		dump($dataCount);die();
 		foreach ($list as $k=>$v) {
 			//直属上级
 			$list[$k]['status_name'] = $v['status']=='1'?'启用':'禁用';
