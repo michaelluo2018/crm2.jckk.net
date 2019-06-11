f9feb55bc6214ab0db750635229b620b68386e38
Xmzz <2253948021@qq.com>
2019-06-11 14:40:50 +0800

    更新

--- application/crm/model/ReceivablesPlan.php
+++ application/crm/model/ReceivablesPlan.php
@@ -98,7 +98,7 @@ class ReceivablesPlan extends Common
 		}
 		if ($param['file_ids']) $param['file'] = arrayToString($param['file_ids']); //附件
 		//期数规则（1,2,3..）
-		$maxNum = db('crm_receivables_plan')->where(['contract_id' => $param['contract_id']])->max('num');
+		$maxNum = db('crm_receivables_plan')->where(['contract_id' => $param['contract_id']])->count('num');
 		halt($param['contract_id'].'<br>'.$maxNum);
 		$param['num'] = $maxNum ? $maxNum+1 : 1;
 		//提醒日期
