a77e619d26ee581a1e84b2a4611a66ef1a92384e
Xmzz <2253948021@qq.com>
2019-07-18 17:07:53 +0800

    更新

--- application/crm/model/ReceivablesPlan.php
+++ application/crm/model/ReceivablesPlan.php
@@ -49,7 +49,9 @@ class ReceivablesPlan extends Common
             $map = where_arr($map, 'crm', 'receivables_plan', 'index'); //高级筛选
         }
         if ($map['receivables_plan.owner_user_id']) {
-            $map['contract.owner_user_id'] = $map['receivables_plan.owner_user_id'];
+            $maps['contract.owner_user_id'] = $map['receivables_plan.owner_user_id'];
+            $maps['contract.ro_user_id'] = ['like','%,'.$request['map']['owner_user_id'].',%'];
+            $maps['contract.rw_user_id'] = ['like','%,'.$request['map']['owner_user_id'].',%'];
             unset($map['receivables_plan.owner_user_id']);
         }
         $list = db('crm_receivables_plan')
