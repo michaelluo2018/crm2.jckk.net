c91673dd6a37e80982559b382671444c0447367f
Xmzz <2253948021@qq.com>
2019-07-18 15:49:57 +0800

    更新

--- application/crm/model/ReceivablesPlan.php
+++ application/crm/model/ReceivablesPlan.php
@@ -51,7 +51,7 @@ class ReceivablesPlan extends Common
         if ($map['receivables_plan.owner_user_id']) {
             $map['contract.owner_user_id'] = $map['receivables_plan.owner_user_id'];
             $maps['contract.ro_user_id'] = ['like','%,'.$request['map']['owner_user_id'].',%'];
-            $mapss['contract.rw_user_id'] = ['like','%,'.$request['map']['owner_user_id'].',%'];
+            $maps['contract.rw_user_id'] = ['like','%,'.$request['map']['owner_user_id'].',%'];
             unset($map['receivables_plan.owner_user_id']);
         }
         $list = db('crm_receivables_plan')
@@ -59,6 +59,8 @@ class ReceivablesPlan extends Common
             ->join('__CRM_CONTRACT__ contract','receivables_plan.contract_id = contract.contract_id','LEFT')
             ->join('__CRM_CUSTOMER__ customer','receivables_plan.customer_id = customer.customer_id','LEFT')
             ->where($map)
+            ->whereOr($maps)
+            ->whereOr($maps)
             ->limit(($request['page']-1)*$request['limit'], $request['limit'])
             ->field('receivables_plan.*,customer.name as customer_name,contract.name as contract_name')
             ->select();
@@ -67,6 +69,8 @@ class ReceivablesPlan extends Common
             ->join('__CRM_CONTRACT__ contract','receivables_plan.contract_id = contract.contract_id','LEFT')
             ->join('__CRM_CUSTOMER__ customer','receivables_plan.customer_id = customer.customer_id','LEFT')
             ->where($map)
+            ->whereOr($maps)
+            ->whereOr($maps)
             ->count('plan_id');
         foreach ($list as $k=>$v) {
             $list[$k]['create_user_id_info'] = $userModel->getUserById($v['create_user_id']);
