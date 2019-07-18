c942613a99a3ab6896971c484a5df48bfdef6394
Xmzz <2253948021@qq.com>
2019-07-18 16:19:51 +0800

    更新

--- application/crm/model/ReceivablesPlan.php
+++ application/crm/model/ReceivablesPlan.php
@@ -48,7 +48,6 @@ class ReceivablesPlan extends Common
         } else {
             $map = where_arr($map, 'crm', 'receivables_plan', 'index'); //高级筛选
         }
-        halt($request);
         if ($map['receivables_plan.owner_user_id']) {
             $maps['contract.owner_user_id'] = [
                 $map['receivables_plan.owner_user_id'],
@@ -66,6 +65,16 @@ class ReceivablesPlan extends Common
             ->limit(($request['page']-1)*$request['limit'], $request['limit'])
             ->field('receivables_plan.*,customer.name as customer_name,contract.name as contract_name')
             ->select();
+        echo db('crm_receivables_plan')
+            ->alias('receivables_plan')
+            ->join('__CRM_CONTRACT__ contract','receivables_plan.contract_id = contract.contract_id','LEFT')
+            ->join('__CRM_CUSTOMER__ customer','receivables_plan.customer_id = customer.customer_id','LEFT')
+            ->where($map)
+            ->whereOr($maps)
+            ->limit(($request['page']-1)*$request['limit'], $request['limit'])
+            ->field('receivables_plan.*,customer.name as customer_name,contract.name as contract_name')
+            ->getlastsql();
+        die();
         $dataCount = db('crm_receivables_plan')
             ->alias('receivables_plan')
             ->join('__CRM_CONTRACT__ contract','receivables_plan.contract_id = contract.contract_id','LEFT')
