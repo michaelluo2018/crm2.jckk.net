79d6b00d2bf1542155614ae613e36ca315012bfa
Xmzz <2253948021@qq.com>
2019-07-18 13:45:06 +0800

    更新

--- application/crm/model/ReceivablesPlan.php
+++ application/crm/model/ReceivablesPlan.php
@@ -53,6 +53,7 @@ class ReceivablesPlan extends Common
             $map['contract.owner_user_id'] = $map['receivables_plan.owner_user_id'];
             unset($map['receivables_plan.owner_user_id']);
         }
+        halt($map);
         $list = db('crm_receivables_plan')
             ->alias('receivables_plan')
             ->join('__CRM_CONTRACT__ contract','receivables_plan.contract_id = contract.contract_id','LEFT')
