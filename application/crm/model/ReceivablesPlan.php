4a0050a9152d01bba1186aa9b4d7c2c0b1e21c7e
Xmzz <2253948021@qq.com>
2019-07-18 11:55:00 +0800

    更新

--- application/crm/model/ReceivablesPlan.php
+++ application/crm/model/ReceivablesPlan.php
@@ -67,6 +67,7 @@ class ReceivablesPlan extends Common
             ->join('__CRM_CUSTOMER__ customer','receivables_plan.customer_id = customer.customer_id','LEFT')
             ->where($map)
             ->count('plan_id');
+        halt($list);
         foreach ($list as $k=>$v) {
             halt($v);
             $list[$k]['create_user_id_info'] = $userModel->getUserById($v['create_user_id']);
